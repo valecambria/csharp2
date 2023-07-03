f35877dfa3250716ccd13bcc700e7b1a8244851b
vale <vale.cambria@hotmail.com>
2023-07-03 11:28:26 -0300

    practica de operadores y valores en C#

---
+++ Program2.cs
@@ -0,0 +1,45 @@
+namespace Ejercicios
+{
+    class Program2 
+    {
+        public static void Main()
+        {
+/*             Console.WriteLine("Ingrese su nombre");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su apellido");
+            string apellido =  Console.ReadLine();
+            Console.WriteLine("Ingrese su edad");
+            int edad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sabe programar? Si / No");
+            string programa =  Console.ReadLine();
+            Console.WriteLine("Hola, " + nombre + " " + apellido + " tenes " + edad + " años y " + programa + " sabes programar"); */
+
+        //* Anotando las caracteristicas principales de 2 objetos utilizando los tipos de datos 
+        // *Coche
+/*         const char puertas = 5;
+        const char ruedas = 4;
+        const string marca = "Peugeot";
+        string ITVvigente = "Si"; */
+
+        //* Mesa
+/*         float peso = 12.50; //*Kilogramos
+        float largo = 1.8; //*Metros
+        string material = "Madera";
+        string color = "Marron"; */
+
+        //* EJERCICIO 3
+        Console.WriteLine("Ingrese un numero: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingrese un valor de 1 caracter");
+        char val = Convert.ToChar(Console.ReadLine());
+
+        if(num >= 18 && val == 'a'){
+            Console.WriteLine("Ambas son verdaderas");
+        } else if (num >= 18 || val == 'a'){
+            Console.WriteLine("Una de las dos es verdadera");
+        } else {
+            Console.WriteLine("No se cumple ninguna de las 2");
+        }
+        }
+    }
+}
